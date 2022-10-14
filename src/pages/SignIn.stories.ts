ebbb8b05e140fb79adaf3364552febf1d8f2f602
Lucas Cavalcante <91428845+CtsLucas@users.noreply.github.com>
2022-10-14 10:49:42 -0300

    feat: add jest, axios and msw

---
+++ src/pages/SignIn.stories.ts
@@ -0,0 +1,43 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { within, userEvent, waitFor } from "@storybook/testing-library";
+import { expect } from "@storybook/jest";
+import { rest } from "msw";
+
+import { SignIn } from "./SignIn";
+
+export default {
+  title: "Pages/SignIn",
+  component: SignIn,
+  args: {},
+  argTypes: {},
+  parameters: {
+    msw: {
+      handlers: [
+        rest.post("/sessions", (req, res, ctx) => {
+          return res(ctx.json({
+            message: "Login successful",
+          }))
+        }),
+      ],
+    },
+  },
+} as Meta;
+
+export const Default: StoryObj = {
+  play: async ({ canvasElement }) => {
+    const { getByPlaceholderText, getByRole, getByText } =
+      within(canvasElement);
+
+    userEvent.type(
+      getByPlaceholderText("Digite seu e-mail"),
+      "lucas@email.com.br"
+    );
+    userEvent.type(getByPlaceholderText("********"), "12345678");
+
+    userEvent.click(getByRole("button"));
+
+    await waitFor(() => {
+      return expect(getByText("Login realizado!")).toBeInTheDocument();
+    });
+  },
+};
